148a005bab935d9391c06af6dc0c996abb4bd275
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-05-09 11:56:45 -0300

    Página de filmes

--- src/components/CardFilm/index.jsx
+++ src/components/CardFilm/index.jsx
@@ -1,18 +1,24 @@
-export default function CardFilm(){
+export default function CardFilm({ filme }){
     return (
-        <div className="w-[350px] h-[280px] flex flex-col rounded-lg bg-[#222222]">
-            <div className="w-full h-[70%] rounded-t-lg">
+        <div className="w-[350px] h-[280px] flex flex-col rounded-lg 
+            bg-[#222222] cursor-pointer border border-transparent
+            hover:border-[#9245EBFF] transition-all duration-300 ease-out">
+            <div className="w-full h-[70%] rounded-t-lg relative">
                 <img className="w-full rounded-t-lg object-cover"
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2R5bdHhYNO5a1GyWXgaE15BLUOxgL1W6B0g&s"
-                    alt=""
+                    src={filme.banner}
+                    alt={filme.titulo}
                 />
+                <div className="absolute top-1 left-2 flex text-[#9ca3af]">
+                    <p className="text-[17px] text-green-400 font-semibold">{filme.nota}</p>
+                    <p>/10</p>
+                </div>
             </div>
             <div className="w-full h-[30%] flex flex-col pt-2 px-4">
-                <p className="text-[15px] font-bold">Velozes & Furiosos 5</p>
-                <p className="text-[15px] text-[#8a898c]">Justin Lin</p>
+                <p className="text-[15px] font-bold">{filme.titulo}</p>
+                <p className="text-[15px] text-[#8a898c]">{filme.diretor}</p>
                 <div className="w-full flex justify-between">
-                    <p className="text-[15px] text-[#8a898c]">2011</p>
-                    <div className="px-2 py-0 bg-[#9245EBFF]/10 text-[#9245EBFF] rounded-lg">Ação</div>
+                    <p className="text-[15px] text-[#8a898c]">{filme.ano}</p>
+                    <div className="px-2 py-0 bg-[#9245EBFF]/10 text-[#9245EBFF] rounded-lg">{filme.genero}</div>
                 </div>
             </div>
         </div>
---
+++ src/pages/filme/[id].jsx
@@ -0,0 +1,24 @@
+import PageWrapper from "@/components/PageWrapper"
+import { useRouter } from "next/router"
+import { FaArrowLeft } from "react-icons/fa";
+
+export default function Filme(){
+    const router = useRouter() 
+    const { id } = router.query  
+
+    return (
+        <PageWrapper
+            showHeader={false}
+        >
+            <div className="w-full h-full flex flex-col px-[350px] pt-12 items-start">
+                <button
+                    onClick={() => router.back()} 
+                    className="px-5 py-2 text-[#8a898c] gap-2 flex items-center 
+                    justify-center rounded-lg hover:bg-[#27282B] hover:text-[#8F7BD8]">
+                    <FaArrowLeft />
+                    <p>Voltar para lista de filmes</p>
+                </button>
+            </div>
+        </PageWrapper>
+    )
+}
